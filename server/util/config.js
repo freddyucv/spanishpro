2f13a3f253009cc5238e8ed23d7e99c4e71ef47a
freddyucv <freddy0309@gmail.com>
2015-07-12 15:53:54 -0430

    Arreglos de explorer

--- server/util/config.js
+++ server/util/config.js
@@ -1,4 +1,4 @@
 module.exports = {
         //rootPath: 'http://localhost:1234/spanishpro'    
-        rootPath: 'http://spanishpro.herokuapp.com/spanishpro'    
+        rootPath: 'http://spanishpro.net'    
 }
--- server/util/db_util.js
+++ server/util/db_util.js
@@ -7,15 +7,6 @@ var MongoClient = require('mongodb').MongoClient;
 //var uri = "mongodb://localhost:27017/spanishproDB";
 //var uri = "mongodb://spanishpro:spanishpro@dbh04.mongolab.com:27047/spanishprodb";
 var uri = "mongodb://spanishpro:spanishpro@ds047802.mongolab.com:47802/spanishprodb";
-//var uri = "mongodb://spanishpro:spanishpro@apollo.modulusmongo.net:27017/ururYq7u";
-
-/*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  uri = "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
-}*/
 
 console.log("mongodb " + uri);
 module.exports = {
